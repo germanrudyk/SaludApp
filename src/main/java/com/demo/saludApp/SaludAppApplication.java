689fa46fd5293c7716d1b5daaa18e4ce963efe57
German Rudyk <germanrudyk87@gmail.com>
2023-05-10 19:25:24 -0300

    montaje de proyecto

---
+++ src/main/java/com/demo/saludApp/SaludAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.saludApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/saludApp/SaludAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.saludApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
