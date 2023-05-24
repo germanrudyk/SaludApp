0850dc38c23f7d387b96c0208ade7606a9fc884c
Ignacio Moreno <nachomoreno3110@gmail.com>
2023-05-24 17:47:07 -0300

    CRUD Cosnulta

--- src/main/java/com/demo/saludApp/servicios/ConsultaServicio.java
+++ src/main/java/com/demo/saludApp/servicios/ConsultaServicio.java
@@ -1,17 +1,20 @@
 package com.demo.saludApp.servicios;
 
 import com.demo.saludApp.entidades.Consulta;
+import com.demo.saludApp.entidades.Imagen;
 import com.demo.saludApp.entidades.Paciente;
 import com.demo.saludApp.entidades.Profesional;
-import com.demo.saludApp.enumeraciones.Especialidad;
 import com.demo.saludApp.enumeraciones.Estado;
 import com.demo.saludApp.enumeraciones.Horario;
 import com.demo.saludApp.enumeraciones.Modalidad;
 import com.demo.saludApp.excepciones.MiException;
 import com.demo.saludApp.repositorios.ConsultaRepositorio;
+import com.demo.saludApp.repositorios.PacienteRepositorio;
+import com.demo.saludApp.repositorios.ProfesionalRepositorio;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,9 +25,15 @@ public class ConsultaServicio {
     @Autowired
     private ConsultaRepositorio consultaRepositorio;
 
-    public void crearConsulta(String fecha, Horario horario, Profesional profesional, Modalidad modalidad, Especialidad especialidad, Integer precio) throws MiException, ParseException {
+    @Autowired
+    private ProfesionalRepositorio profesionalRepositorio;
 
-        validar(fecha, horario, profesional, modalidad, especialidad, precio);
+    @Autowired
+    private PacienteRepositorio pacienteRepositorio;
+
+    public void crearConsulta(String fecha, Horario horario, Profesional profesional, Modalidad modalidad, Double precio) throws MiException, ParseException {
+
+        validar(fecha, horario, profesional, modalidad, precio);
 
         Consulta consulta = new Consulta();
 
@@ -35,8 +44,9 @@ public class ConsultaServicio {
         consulta.setFechaConsulta(fechaConsulta);
 
         consulta.setHorario(horario);
-//        consulta.setMatriculaProfesional(profesional);
-//        consulta.setPrecioConsulta(precio);
+        consulta.setProfesional(profesional);
+        consulta.setModalidad(modalidad);
+        consulta.setPrecioConsulta(precio);
 
         consultaRepositorio.save(consulta);
 
@@ -46,12 +56,11 @@ public class ConsultaServicio {
 
         Optional<Consulta> respuesta = consultaRepositorio.findById(idConsulta);
 
-        if (respuesta.isPresent()) {
+        if (respuesta.isPresent() || !(paciente.toString().isEmpty()) || !(paciente == null)) {
             Consulta consulta = respuesta.get();
 
-//            consulta.setDniPaciente(paciente);
-            consulta.setObrasocial(paciente.getObraSocial());
-
+            consulta.setPaciente(paciente);
+            consulta.setObrasocial(paciente.getObraSocial()); // se guarda la obra social porque puede cambiar en el tiempo
             consulta.setEstado(Estado.RESERVADA);
 
             consultaRepositorio.save(consulta);
@@ -59,7 +68,87 @@ public class ConsultaServicio {
 
     }
 
-    private void validar(String fecha, Horario horario, Profesional profesional, Modalidad modalidad, Especialidad especialidad, Integer precio) throws MiException {
+    public void realizarConsulta(String idConsulta, String detalleConsulta, Imagen estudio) {
+
+        Optional<Consulta> respuesta = consultaRepositorio.findById(idConsulta);
+
+        if (respuesta.isPresent() || !(detalleConsulta.isEmpty()) || !(detalleConsulta == null) || !(estudio == null)) {
+
+            Consulta consulta = respuesta.get();
+
+            consulta.setDetalleConsulta(detalleConsulta);
+
+            List<Imagen> lista = consulta.getEstudios();
+            lista.add(estudio);
+            consulta.setEstudios(lista);
+
+            consulta.setEstado(Estado.UTILIZADA);
+
+            consultaRepositorio.save(consulta);
+        }
+
+    }
+
+    public List<Consulta> buscarConsultaPorFecha(Date fecha) {
+        return consultaRepositorio.buscarPorFecha(fecha);
+    }
+
+    public List<Consulta> buscarConsultaPorProfesional(String idProfesional) {
+        Optional<Profesional> respuesta = profesionalRepositorio.findById(idProfesional);
+
+        if (respuesta.isPresent()) {
+            Profesional profesional = respuesta.get();
+            return consultaRepositorio.buscarPorProfesional(profesional);
+        }
+        return null;        // ver opciones
+    }
+
+    public List<Consulta> buscarConsultaPorPaciente(String idPaciente) {
+        Optional<Paciente> respuesta = pacienteRepositorio.findById(idPaciente);
+
+        if (respuesta.isPresent()) {
+            Paciente paciente = respuesta.get();
+            return consultaRepositorio.buscarTodoPorPaciente(paciente);
+        }
+        return null;        // ver opciones
+    }
+
+    public List<Consulta> buscarConsultaPorPacientePorFecha(String idPaciente, Date fecha) {
+        Optional<Paciente> respuesta = pacienteRepositorio.findById(idPaciente);
+
+        if (respuesta.isPresent() && fecha != null) {
+            Paciente paciente = respuesta.get();
+            return consultaRepositorio.buscarPorPacientePorFecha(paciente, fecha);
+        }
+        return null;        // ver opciones
+    }
+
+    public List<Consulta> buscarPorEstado(Estado estado) {
+        return consultaRepositorio.buscarPorEstado(estado);
+    }
+
+    public List<Consulta> buscarPorPrecio(Double precio) {
+        if (precio != null) {
+            return consultaRepositorio.buscarPorPrecio(precio);
+        }
+        return null;
+    }
+
+    public void darBajaConsulta(String idConsulta) {
+        Optional<Consulta> respuesta = consultaRepositorio.findById(idConsulta);
+
+        if (respuesta.isPresent()) {
+            Consulta consulta = respuesta.get();
+
+            consulta.setEstado(Estado.CANCELADA);
+
+            consultaRepositorio.save(consulta);
+        }
+
+    }
+
+
+private void validar(String fecha, Horario horario, Profesional profesional, Modalidad modalidad, Double precio) throws MiException {
 
         if (fecha.isEmpty() || fecha == null) {
             throw new MiException("La fecha no puede ser nulo o estar vacio"); //
@@ -72,9 +161,6 @@ public class ConsultaServicio {
         }
         if (modalidad.toString().isEmpty() || modalidad == null) {
             throw new MiException("la Modalidad no puede ser nulo o estar vacio"); //
-        }
-        if (especialidad.toString().isEmpty() || especialidad == null) {
-            throw new MiException("la especialidad no puede ser nulo o estar vacio"); //
         }
         if (precio == null) {
             throw new MiException("el precio no puede estar vacio"); //
