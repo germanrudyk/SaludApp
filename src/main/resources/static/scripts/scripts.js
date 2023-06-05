0da44dd883d7fade40d35e2eb1c1187eedd309b1
Damian <dellacortedamian@gmail.com>
2023-06-05 19:27:40 -0300

    nada

--- src/main/resources/static/scripts/scripts.js
+++ src/main/resources/static/scripts/scripts.js
@@ -1,77 +1,58 @@
-/*//------------- Ordenar------------- 
-// Obtener la tabla
-var table = document.querySelector('.table');
-
-// Obtener los encabezados de la tabla
-var headers = table.querySelectorAll('.sortable-header');
-
-// Variable para realizar un seguimiento del orden actual
-var sortOrder = {
-    columnIndex: null,
-    ascending: true
-};
-
-// Agregar un evento clic a los encabezados
-headers.forEach(function (header) {
-    header.addEventListener('click', function () {
-        // Obtener el índice de la columna desde el atributo data
-        var columnIndex = parseInt(header.getAttribute('data-column-index'));
-
-        // Verificar si se ha hecho clic en la misma columna nuevamente
-        if (sortOrder.columnIndex === columnIndex) {
-            // Cambiar el orden
-            sortOrder.ascending = !sortOrder.ascending;
-        } else {
-            // Establecer la columna actual y el orden ascendente
-            sortOrder.columnIndex = columnIndex;
-            sortOrder.ascending = true;
-        }
-
-        // Obtener todas las filas de la tabla, excepto la primera (encabezados)
-        var rows = Array.from(table.querySelectorAll('tbody tr')).slice(0);
-
-        // Ordenar las filas en función del valor de la columna seleccionada y el orden actual
-        rows.sort(function (a, b) {
-            var aValue = getRatingValue(a);
-            var bValue = getRatingValue(b);
-
-            // Comparar los valores según el orden ascendente o descendente
-            if (sortOrder.ascending) {
-                return aValue - bValue;
-            } else {
-                return bValue - aValue;
-            }
-        });
-
-        // Función para obtener el valor de calificación de una fila
-        function getRatingValue(row) {
-            var starsInner = row.querySelector('.stars-inner');
-            var starWidth = starsInner.style.width;
-
-            // Obtener el ancho de las estrellas internas como un porcentaje
-            var starPercentage = parseFloat(starWidth.replace('%', ''));
-
-            // Convertir el porcentaje en un valor de calificación entre 0 y 5
-            var ratingValue = (starPercentage / 100) * 5;
-
+/*------------- Ordenar-------------*/
+document.addEventListener('DOMContentLoaded', function() {
+  var table = document.querySelector('.table');
+  var headers = table.querySelectorAll('.sortable-header');
+  // Variable para realizar un seguimiento del orden actual
+  var sortOrder = {
+      columnIndex: null,
+      ascending: true
+  };
+  // Agregar un evento clic a los encabezados
+  headers.forEach(function (header) {
+      header.addEventListener('click', function () {
+          // Obtener el índice de la columna desde el atributo data
+          var columnIndex = parseInt(header.getAttribute('data-column-index'));
+          // Verificar si se ha hecho clic en la misma columna nuevamente
+          if (sortOrder.columnIndex === columnIndex) {
+              // Cambiar el orden
+              sortOrder.ascending = !sortOrder.ascending;
+          } else {
+              // Establecer la columna actual y el orden ascendente
+              sortOrder.columnIndex = columnIndex;
+              sortOrder.ascending = true;
+          }
+          // Obtener todas las filas de la tabla, excepto la primera (encabezados)
+          var rows = Array.from(table.querySelectorAll('tbody tr')).slice(0);
+          // Ordenar las filas en función del valor de la columna seleccionada y el orden actual
+          rows.sort(function (a, b) {
+              var aValue = getRatingValue(a);
+              var bValue = getRatingValue(b);
+              // Comparar los valores según el orden ascendente o descendente
+              if (sortOrder.ascending) {
+                  return aValue - bValue;
+              } else {
+                  return bValue - aValue;
+              }
+          });
+          // Función para obtener el valor del precio de una fila
+          function getRatingValue(row) {
+            const Precios = row.querySelector('.precios');
+            const ratingValue = parseFloat(Precios.innerText);
             return ratingValue;
-        }
-
-        // Obtener el padre de las filas (el tbody)
-        var parent = rows[0].parentNode;
-
-        // Remover las filas existentes de la tabla
-        rows.forEach(function (row) {
-            parent.removeChild(row);
-        });
-
-        // Agregar las filas ordenadas de nuevo a la tabla
-        rows.forEach(function (row) {
-            parent.appendChild(row);
-        });
-    });
+          }        
+          // Obtener el padre de las filas (el tbody)
+          var parent = rows[0].parentNode;
+          // Remover las filas existentes de la tabla
+          rows.forEach(function (row) {
+              parent.removeChild(row);
+          });
+          // Agregar las filas ordenadas de nuevo a la tabla
+          rows.forEach(function (row) {
+              parent.appendChild(row);
+          });
+      });
+  });
 });
-
 /*------------- Filtrar tabla de turnos -------------*/
 function filtrarTabla(filtro) {
   var filtroElegido = document.querySelector(`.filtro-${filtro}`).value;
